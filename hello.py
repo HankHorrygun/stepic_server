8ccff60c43184500f42fcb4cd47f57953ecd8d3b
HankHorrygun <C:\Users\vladimir\AppData\Roaming\The Bat!>
2016-03-15 17:24:34 +1200

    small changes

--- hello.py
+++ hello.py
@@ -2,8 +2,4 @@
 
 def app(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    #with open('/home/box/web/log.log','a') as f:
-    #    f.write(env['QUERY_STRING'])
-    result = '<br>'.join(env['QUERY_STRING'].split('&'))
-    return [env['QUERY_STRING']]
-    return [result]
+    return [ '%s\n' %x for x in env['QUERY_STRING'].split('&') ]
