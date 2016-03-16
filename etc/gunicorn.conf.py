e15eb0fb75e2dfd537c6be76002503e22f70294b
HankHorrygun <C:\Users\vladimir\AppData\Roaming\The Bat!>
2016-03-16 12:12:29 +1200

    configs for django

--- etc/gunicorn.conf.py
+++ etc/gunicorn.conf.py
@@ -1,5 +1,5 @@
 CONFIG = {
-    'working_dir': '/home/box/web/',
+    'working_dir': '/home/box/web/ask/',
     'args': ( '--bind=0.0.0.0:8080', 
               'hello:app', ),
 }
--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn_django.conf.py   /etc/gunicorn.d/django
 
 sudo /etc/init.d/gunicorn restart
