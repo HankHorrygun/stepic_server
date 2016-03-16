433fd097348325ca39296b149f6e3844a3d6f72f
Hank <hangsmail@gmail.com>
2016-03-16 00:55:58 +0000

    working 2.1 task

--- etc/gunicorn_django.conf.py
+++ etc/gunicorn_django.conf.py
@@ -1,6 +1,5 @@
 CONFIG = {
     'working_dir': '/home/box/web/ask/',
     'args': ( '--bind=0.0.0.0:8000', 
-              '--env DJANGO_SETTINGS_MODULE=ask.settings'
-              'ask:wsgi', ),
+              'ask.wsgi', ),
 }
